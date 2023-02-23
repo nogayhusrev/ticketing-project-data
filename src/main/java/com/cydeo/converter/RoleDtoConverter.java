aa06db1aa071e4efe2c7be95cd0527abdadc2be1
nogayhusrev <nogayhusrev@gmail.com>
2023-02-23 16:11:13 +0300

    part-1 clean up done

--- src/main/java/com/cydeo/converter/RoleDtoConverter.java
+++ src/main/java/com/cydeo/converter/RoleDtoConverter.java
@@ -1,7 +1,6 @@
 package com.cydeo.converter;
 
 import com.cydeo.dto.RoleDTO;
-import com.cydeo.service.RoleService;
 import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
@@ -9,22 +8,26 @@ import org.springframework.stereotype.Component;
 @Component
 @ConfigurationPropertiesBinding
 public class RoleDtoConverter implements Converter<String, RoleDTO> {
-
-    RoleService roleService;
-
-    public RoleDtoConverter(RoleService roleService) {
-        this.roleService = roleService;
-    }
-
     @Override
     public RoleDTO convert(String source) {
-
-        if (source == null || source.equals("")) {  //  Select  -> ""
-            return null;
-        }
-
-        return roleService.findById(Long.parseLong(source));
-
+        return null;
     }
+//
+//    RoleService roleService;
+//
+//    public RoleDtoConverter(RoleService roleService) {
+//        this.roleService = roleService;
+//    }
+//
+//    @Override
+//    public RoleDTO convert(String source) {
+//
+//        if (source == null || source.equals("")) {  //  Select  -> ""
+//            return null;
+//        }
+//
+//        return roleService.findById(Long.parseLong(source));
+//
+//    }
 
 }
--- src/main/java/com/cydeo/converter/UserDtoConverter.java
+++ src/main/java/com/cydeo/converter/UserDtoConverter.java
@@ -1,7 +1,6 @@
 package com.cydeo.converter;
 
 import com.cydeo.dto.UserDTO;
-import com.cydeo.service.UserService;
 //import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
@@ -9,22 +8,26 @@ import org.springframework.stereotype.Component;
 @Component
 //@ConfigurationPropertiesBinding
 public class UserDtoConverter implements Converter<String, UserDTO> {
-
-    UserService userService;
-
-    public UserDtoConverter(UserService userService) {
-        this.userService = userService;
-    }
-
     @Override
     public UserDTO convert(String source) {
-
-        if (source == null || source.equals("")) {
-            return null;
-        }
-
-        return userService.findById(source);
-
+        return null;
     }
+//
+//    UserService userService;
+//
+//    public UserDtoConverter(UserService userService) {
+//        this.userService = userService;
+//    }
+//
+//    @Override
+//    public UserDTO convert(String source) {
+//
+//        if (source == null || source.equals("")) {
+//            return null;
+//        }
+//
+//        return userService.findById(source);
+//
+//    }
 
 }

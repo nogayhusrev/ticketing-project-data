8e830a7393da1461a46409ef49043b3e577beabb
nogayhusrev <nogayhusrev@gmail.com>
2023-02-24 11:06:55 +0300

    part-2 started

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -1,14 +1,26 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.UserDTO;
+import com.cydeo.entity.User;
+import com.cydeo.repository.UserRepository;
 import com.cydeo.service.UserService;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public List<UserDTO> listAllUsers() {
+        List<User> userList = userRepository.findAll(Sort.by("firstName"));
+
         return null;
     }
 
